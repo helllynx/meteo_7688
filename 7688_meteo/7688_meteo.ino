b0ad8dd710c9d21fddac4daed1cd64550b9f4f03
Yanis Vasiliadis <white.night.447@gmail.com>
2023-12-02 15:38:17 +0300

    Libraries added

--- 7688_meteo/7688_meteo.ino
+++ 7688_meteo/7688_meteo.ino
@@ -1,7 +1,5 @@
-#include "DHT.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h>
-#include <SFE_BMP180.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <ArduinoJson.h>
@@ -18,14 +16,27 @@
 #error("Height incorrect, please fix Adafruit_SH1106.h!");
 #endif
 
+#include "DHT.h"
 #define DHTPIN 8 // what digital pin we're connected to
 #define DHTTYPE DHT22
-
 DHT dht(DHTPIN, DHTTYPE);
-Adafruit_SH1106 display(OLED_RESET);
 
+#include "MHZ19.h"
+
+const int rx_pin = 9; //Serial rx pin no
+const int tx_pin = 10; //Serial tx pin no
+
+//const int pwmpin = 14;
+
+MHZ19 *mhz19_uart = new MHZ19(rx_pin,tx_pin);
+//MHZ19 *mhz19_pwm = new MHZ19(pwmpin);
+
+#include <SFE_BMP180.h>
 SFE_BMP180 pressure;
 
+Adafruit_SH1106 display(OLED_RESET);
+
+// CODE
 struct BMP180Data {
   float pressure, temperature;
 };
@@ -50,6 +61,12 @@ void setup() {
 
   }
 
+  mhz19_uart->begin(rx_pin, tx_pin);
+  mhz19_uart->setAutoCalibration(false);
+  delay(3000); // Issue #14
+  Serial.print("MH-Z19 now warming up...  status:");
+  Serial.println(mhz19_uart->getStatus());
+
   // OLED SH1106
   display.begin(SH1106_SWITCHCAPVCC,0x3C); // initialize with the I2C addr 0x3D (for the 128x64)
   display.clearDisplay();
@@ -63,6 +80,7 @@ void loop() {
   // BMP180
   BMP180Data bmp180Data = getPressure();
   double P = bmp180Data.pressure * 0.750062;
+  // end BMP180
 
   // DHT22
   // Reading temperature or humidity takes about 250 milliseconds!
@@ -79,6 +97,14 @@ void loop() {
 
   // Compute heat index in Celsius (isFahreheit = false)
   float tempDHT = dht.computeHeatIndex(t, h, false);
+  // end DHT
+
+  // CO2 MHZ19B
+  measurement_t m = mhz19_uart->getMeasurement();
+  // end CO2
+  
+  
+  delay(3000);
 
   display.clearDisplay();
   // OLED SH1106
@@ -100,10 +126,14 @@ void loop() {
   display.print(P);
   display.println(" mmHG");
 
+  display.print("CO2: ");
+  display.print(m.co2_ppm);
+  display.println(" PPM");
+
   display.display();
   // generate_json(tempDHT, bmp180Data.temperature, P, h);
-  write_csv_to_serial(tempDHT, bmp180Data.temperature, P, h);
-  delay(10000);
+  write_csv_to_serial(tempDHT, bmp180Data.temperature, P, h, m.co2_ppm);
+  delay(30000);
 }
 
 BMP180Data getPressure() {
@@ -174,7 +204,7 @@ BMP180Data getPressure() {
 //   serializeJson(jsonData, Serial1);
 // }
 
-void write_csv_to_serial(float tempDHT, float tempBMP180, double pressure, float humidity) {
+void write_csv_to_serial(float tempDHT, float tempBMP180, double pressure, float humidity, int co2) {
   Serial1.print(millis());
   Serial1.print(F(", "));
   Serial1.print(tempDHT);
@@ -183,5 +213,7 @@ void write_csv_to_serial(float tempDHT, float tempBMP180, double pressure, float
   Serial1.print(F(", "));
   Serial1.print(pressure);
   Serial1.print(F(", "));
-  Serial1.println(humidity);
+  Serial1.print(humidity);
+  Serial1.print(F(", "));
+  Serial1.println(co2);
 }
